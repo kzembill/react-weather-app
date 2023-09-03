48fda8aa44c1fdd7acb85ff1eb6b9618854d092e
kzembill <kzembill@gmail.com>
2023-09-03 11:02:28 -0400

    I had my API working but could not figure out how to have my app show the container even when I do not search a city so I redid the entire thing changes to all components and deleted my Search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Search from "./Search";
+import Search from "./Weather";
 
 
 export default function App() {
   return (
     <div className="App">
-      <Search />
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Search from "./Search.js";
+import Weather from "./Weather";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className= "App">
-    <Search />
+    <Weather />
     </div>
   </React.StrictMode>
 );
